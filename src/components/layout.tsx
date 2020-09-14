a9282cbd6747d1fc35bbbdcd4eb6c33e061685c7
RJ Cabagnot <rjcabagnot@hotmail.com>
2020-09-14 09:19:46 +0200

    updated

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,14 +5,18 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
+import React, { ReactNode } from "react"
+
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
 
-const Layout = ({ children }) => {
+interface Props {
+  children: ReactNode
+}
+
+const Layout = ({ children }: Props) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
---
+++ src/components/name_form.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+export default class NameForm extends React.Component {
+    render() {
+        return (
+            <div>
+                <form> 
+                    <input type="text"/>
+                    <button>Submit </button>
+                </form>
+            </div>
+        );
+    }
+}
+
